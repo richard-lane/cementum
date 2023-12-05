57133e6179626802ea8555ff01392c9e4957128a
Richard Lane <richard.lane@bristol.ac.uk>
2023-12-05 10:37:08 +0000

    data augmentation fcn

--- pipeline/cementum/processing.py
+++ pipeline/cementum/processing.py
@@ -41,3 +41,37 @@ def resample_by_interpolation(
         np.linspace(0.0, 1.0, len(signal), endpoint=False),  # known positions
         signal,  # known data points
     )
+
+
+def rotate_and_flip(
+    generator: np.random.Generator, images: np.ndarray, masks: np.ndarray
+) -> tuple[np.ndarray, np.ndarray]:
+    """
+    Randomly rotate and flip the images and masks.
+
+    :param generator: The random number generator to use.
+    :param images: The images to rotate and flip.
+    :param masks: The masks to rotate and flip.
+
+    """
+    assert len(images) == len(masks), "images and masks must have the same length"
+
+    # Generate random numbers of rotations and flips
+    rotations = generator.integers(0, 4, size=len(images))
+    flips = generator.choice([True, False], size=len(images), replace=True)
+
+    # Initialize arrays to hold the rotated and flipped images and masks
+    rotated_images = np.empty_like(images)
+    rotated_masks = np.empty_like(masks)
+
+    for i in range(len(images)):
+        # Rotate
+        rotated_images[i] = np.rot90(images[i], rotations[i], axes=(0, 1))
+        rotated_masks[i] = np.rot90(masks[i], rotations[i], axes=(0, 1))
+
+        # Flip
+        if flips[i]:
+            rotated_images[i] = np.flip(rotated_images[i], axis=1)
+            rotated_masks[i] = np.flip(rotated_masks[i], axis=1)
+
+    return rotated_images, rotated_masks
